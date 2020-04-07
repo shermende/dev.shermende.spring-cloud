b2f3b575ee7f9f671d4785460b7fd8b134bd446a
abdys <n.u.abdysamat@gmail.com>
2020-04-07 11:02:53 +0600

    refactor

---
+++ app-translation/src/main/java/dev/shermende/translation/db/entity/Translate.java
@@ -0,0 +1,32 @@
+package dev.shermende.translation.db.entity;
+
+import dev.shermende.lib.db.entity.TimedEntity;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Translate extends TimedEntity<Long> {
+
+    private static final long serialVersionUID = -5368034288441153128L;
+
+    @NotEmpty
+    @Size(min = 2, max = 2)
+    private String locale;
+
+    @NotEmpty
+    @Size(max = 255)
+    private String key;
+
+    @NotEmpty
+    @Size(max = 4096)
+    private String value;
+
+}
---
+++ app-translation/src/main/java/dev/shermende/translation/db/repository/TranslateRepository.java
@@ -0,0 +1,25 @@
+package dev.shermende.translation.db.repository;
+
+import dev.shermende.lib.db.repository.QueryDslRepository;
+import dev.shermende.translation.db.entity.QTranslate;
+import dev.shermende.translation.db.entity.Translate;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
+
+@RepositoryRestResource
+public interface TranslateRepository extends QueryDslRepository<Translate, Long, QTranslate> {
+
+    @Override
+    @Query("SELECT e FROM Translate e WHERE e.id = :id")
+    Optional<Translate> findWithDetails(Long id);
+
+    @Override
+    default void customize(@NotNull QuerydslBindings bindings, @NotNull QTranslate root) {
+
+    }
+
+}
