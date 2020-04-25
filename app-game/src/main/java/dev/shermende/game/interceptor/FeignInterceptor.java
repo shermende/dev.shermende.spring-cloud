1967409c1bce9bd7828f9850e29013c67431d57c
abdys <n.u.abdysamat@gmail.com>
2020-04-25 17:16:50 +0600

    sba add log.file

--- app-game/src/main/java/dev/shermende/game/interceptor/FeignInterceptor.java
+++ app-game/src/main/java/dev/shermende/game/interceptor/FeignInterceptor.java
@@ -1,11 +1,8 @@
 package dev.shermende.game.interceptor;
 
-import dev.shermende.lib.model.authorization.UserModel;
 import dev.shermende.support.spring.component.Interceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Slf4j
 @Component
@@ -19,8 +16,7 @@ public class FeignInterceptor implements Interceptor {
     @Override
     @SuppressWarnings("unchecked")
     public void intercept(Object o) {
-        Optional<UserModel> userModel = (Optional<UserModel>) o;
-        log.info("{}", userModel);
+        log.info("{}", o);
     }
 
 }
