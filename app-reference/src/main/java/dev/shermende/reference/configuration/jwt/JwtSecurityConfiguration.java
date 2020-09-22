1f68ffe2ee9786b67dee17226429de86f278dc89
abdys <n.u.abdysamat@gmail.com>
2020-09-22 15:53:55 +0600

    rsa key for jwt

--- app-reference/src/main/java/dev/shermende/reference/configuration/jwt/JwtSecurityConfiguration.java
+++ app-reference/src/main/java/dev/shermende/reference/configuration/jwt/JwtSecurityConfiguration.java
@@ -1,10 +1,11 @@
 package dev.shermende.reference.configuration.jwt;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import dev.shermende.lib.support.dal.util.RSA;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,20 +14,27 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotEmpty;
-import java.math.BigInteger;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPublicKeySpec;
 
 @Configuration
 @Profile({"jwt"})
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class JwtSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public RSAPublicKey rsaPublicKey(
+        @Value("classpath:jwt/public.pem") Resource publicKeyFile
+    ) throws GeneralSecurityException, IOException {
+        return RSA.getPublicKey(publicKeyFile.getFile().getAbsolutePath());
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RSAPublicKey rsaPublicKey) {
+        return NimbusJwtDecoder.withPublicKey(rsaPublicKey).build();
+    }
 
     @Bean
     public AppJwtAuthenticationConverter appJwtAuthenticationConverter() {
@@ -53,34 +61,6 @@ public class JwtSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .oauth2ResourceServer().jwt()
             .jwtAuthenticationConverter(jwt -> appJwtAuthenticationConverter().convert(jwt))
         ;
-    }
-
-    @Bean
-    @Validated
-    @ConfigurationProperties("jwt-properties")
-    public KeyProperties keyProperties() {
-        return new KeyProperties();
-    }
-
-    @Bean
-    public RSAPublicKey rsaPublicKey(
-        KeyProperties properties
-    ) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(
-            new RSAPublicKeySpec(new BigInteger(properties.getModulus()), new BigInteger(properties.getExponent())));
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder(RSAPublicKey rsaPublicKey) {
-        return NimbusJwtDecoder.withPublicKey(rsaPublicKey).build();
-    }
-
-    @Data
-    public static class KeyProperties {
-        @NotEmpty
-        private String modulus = "18044398961479537755088511127417480155072543594514852056908450877656126120801808993616738273349107491806340290040410660515399239279742407357192875363433659810851147557504389760192273458065587503508596714389889971758652047927503525007076910925306186421971180013159326306810174367375596043267660331677530921991343349336096643043840224352451615452251387611820750171352353189973315443889352557807329336576421211370350554195530374360110583327093711721857129170040527236951522127488980970085401773781530555922385755722534685479501240842392531455355164896023070459024737908929308707435474197069199421373363801477026083786683";
-        @NotEmpty
-        private String exponent = "65537";
     }
 
 }
